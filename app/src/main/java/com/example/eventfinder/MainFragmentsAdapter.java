0823b099dc6a9af0b36de9f47434699609f71edf
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-04-15 20:46:56 -0700

    Adapter to bind data between search and fav fragments with the main activity

---
+++ app/src/main/java/com/example/eventfinder/MainFragmentsAdapter.java
@@ -0,0 +1,37 @@
+package com.example.eventfinder;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class MainFragmentsAdapter extends FragmentStateAdapter {
+
+    private final ArrayList<Fragment> fragmentArrayList = new ArrayList<>();
+
+    public MainFragmentsAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle){
+        super(fragmentManager, lifecycle);
+    }
+
+    public void addFragment(Fragment fragment){
+        fragmentArrayList.add(fragment);
+    }
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+
+        return fragmentArrayList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragmentArrayList.size();
+    }
+
+
+}
