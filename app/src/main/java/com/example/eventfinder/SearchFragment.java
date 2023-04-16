eea3dc43f0642b333bedbe33ff8bdfced13ee86a
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-04-16 15:37:09 -0700

    Search Fragment with partial search and clear functionalities.

--- app/src/main/java/com/example/eventfinder/SearchFragment.java
+++ app/src/main/java/com/example/eventfinder/SearchFragment.java
@@ -6,24 +6,59 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.SyncStateContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment #newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.example.eventfinder.DataClasses.SearchObject;
+import com.example.eventfinder.Helpers.ServerAccessHelper;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class SearchFragment extends Fragment {
 
-    MainFragmentsAdapter mainFragmentsAdapter;
+    private EditText keywordET;
+    private EditText distanceET;
+    private EditText locationET;
+
+    private Spinner categorySpinner;
+    private Switch autoDetect;
+
+    private RequestQueue requestQueue;
+
+    private ServerAccessHelper serverAccessHelper;
+
+    Map<String, String> category_dictionary;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        requestQueue = Volley.newRequestQueue(getActivity());
+        serverAccessHelper = new ServerAccessHelper(requestQueue);
+
+        category_dictionary = new HashMap<>();
+
+        // Add the key-value pairs to the dictionary
+        category_dictionary.put("All", "Default");
+        category_dictionary.put("Music", "KZFzniwnSyZfZ7v7nJ");
+        category_dictionary.put("Sports", "KZFzniwnSyZfZ7v7nE");
+        category_dictionary.put("Arts & Theatre", "KZFzniwnSyZfZ7v7na");
+        category_dictionary.put("Film", "KZFzniwnSyZfZ7v7nn");
+        category_dictionary.put("Miscellaneous", "KZFzniwnSyZfZ7v7n1");
 
         return inflater.inflate(R.layout.fragment_search, container, false);
 
@@ -33,8 +68,82 @@ public class SearchFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        String[] categoriesList = getResources().getStringArray(R.array.categories_list);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), R.layout.spinner_style, categoriesList);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.categories_list, R.layout.spinner_style);
+        adapter.setDropDownViewResource(R.layout.spinner_style);
+        categorySpinner.setAdapter(adapter);
+
+        keywordET = view.findViewById(R.id.keyword_edittext);
+        distanceET = view.findViewById(R.id.distance_edittext);
+        locationET = view.findViewById(R.id.location_edittext);
+
+        categorySpinner = view.findViewById(R.id.spinner_categories);
+
+        Button searchButton = view.findViewById(R.id.search_button);
+        searchButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                searchButtonClicked(view);
+            }
+        });
+
+        Button clearButton = view.findViewById(R.id.clear_button);
+        clearButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                clearButtonClicked(view);
+            }
+        });
+
+        autoDetect = view.findViewById(R.id.switch_autodetect);
+        autoDetect.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                if(isChecked){
+                    locationET.setVisibility(View.GONE);
+                }else{
+                    locationET.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+    private void clearButtonClicked(View view) {
+        keywordET.setText("");
+        distanceET.setText("10");
+        locationET.setText("");
+        autoDetect.setChecked(false);
+        locationET.setVisibility(View.VISIBLE);
+        categorySpinner.setSelection(0);
+    }
+
+    private boolean validateForm(){
+        String keyword = keywordET.getText().toString();
+        String distance = distanceET.getText().toString();
+        String location = locationET.getText().toString();
+        boolean auto_detect_loc = autoDetect.isChecked();
+
+
+        if(keyword.trim().length() == 0 || distance.trim().length() == 0 || (location.trim().length() == 0) && !auto_detect_loc) {
+            Snackbar.make(getActivity().findViewById(R.id.viewPager_mainActivity), "Please fill all fields", Snackbar.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
+    }
+
+    private void searchButtonClicked(View view) {
+
+        if(validateForm()){
+
+            Toast.makeText(getActivity(), "Search called", Toast.LENGTH_SHORT).show();
+            String category = categorySpinner.getSelectedItem().toString();
+
+            category = category_dictionary.get(category);
+
+            SearchObject searchObject = new SearchObject(keywordET.getText().toString(), category, distanceET.getText().toString(), 34.003, -118.2863);
+
+            serverAccessHelper.search(searchObject, getActivity());
+
+        }
     }
 }
