d9cfe405c820038f812f526db161d23a41fbef70
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-04-17 10:53:22 -0700

    Location Model

--- app/src/main/java/com/example/eventfinder/DataClasses/Location.java
+++ app/src/main/java/com/example/eventfinder/DataClasses/Location.java
@@ -1,6 +1,39 @@
 package com.example.eventfinder.DataClasses;
 
-public class Location {
-    protected String address;
+import androidx.annotation.NonNull;
 
+import org.json.JSONObject;
+
+public class Location {
+    private double latitude;
+    private double longitude;
+
+    public Location(String loc) {
+        String[] locParts = loc.split(",");
+        this.latitude = Double.parseDouble(locParts[0]);
+        this.longitude = Double.parseDouble(locParts[1]);
+    }
+
+    public Location(JSONObject loc){
+        try {
+            this.latitude = loc.getDouble("lat");
+            this.longitude = loc.getDouble("lng");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return String.format("%s, %s", this.latitude, this.longitude);
+    }
 }
