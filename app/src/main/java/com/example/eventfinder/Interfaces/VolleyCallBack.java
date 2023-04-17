7a3ecf18e6b6294ec1a0792979c2fcceacbcd144
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-04-17 10:54:20 -0700

    VolleyCallBack interface

---
+++ app/src/main/java/com/example/eventfinder/Interfaces/VolleyCallBack.java
@@ -0,0 +1,8 @@
+package com.example.eventfinder.Interfaces;
+//https://stackoverflow.com/questions/49342841/android-wait-for-volley-response-for-continue
+
+import com.example.eventfinder.DataClasses.Location;
+
+public interface VolleyCallBack {
+    void onSuccess(Location location);
+}
