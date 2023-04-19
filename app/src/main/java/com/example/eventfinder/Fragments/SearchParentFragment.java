ec86bed4794eb0869f9b92647e281a41467496a9
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-04-18 17:36:51 -0700

    [NEW] Search Parent Fragment

---
+++ app/src/main/java/com/example/eventfinder/Fragments/SearchParentFragment.java
@@ -0,0 +1,38 @@
+package com.example.eventfinder.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.eventfinder.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SearchParentFragment #newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SearchParentFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_search_parent, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
