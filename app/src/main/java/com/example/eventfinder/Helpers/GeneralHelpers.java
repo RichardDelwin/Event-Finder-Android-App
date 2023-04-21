e78186c97039396c6ce19b5e6a1d3e8d659a9bbf
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-04-20 21:20:48 -0700

    [BUGFIX] Handles null cases

--- app/src/main/java/com/example/eventfinder/Helpers/GeneralHelpers.java
+++ app/src/main/java/com/example/eventfinder/Helpers/GeneralHelpers.java
@@ -7,12 +7,16 @@ import java.time.format.DateTimeFormatter;
 public class GeneralHelpers {
 
     public static String getFormattedDate(String localDate){
+        if(localDate==null)
+            return "";
         LocalDate date = LocalDate.parse(localDate);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         return date.format(formatter);
     }
 
     public static String getFormattedTime(String localTime) {
+        if(localTime==null)
+            return "";
         LocalTime time = LocalTime.parse(localTime);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("h:mm a");
         return time.format(formatter);
