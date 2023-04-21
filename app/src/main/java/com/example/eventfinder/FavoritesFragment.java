e5dfa97bea3d8d885aa8504106496a4ccfb3b58c
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-04-20 21:20:02 -0700

    [NEW] All functionalities are implemented

--- app/src/main/java/com/example/eventfinder/FavoritesFragment.java
+++ app/src/main/java/com/example/eventfinder/FavoritesFragment.java
@@ -2,63 +2,117 @@ package com.example.eventfinder;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.eventfinder.Adapters.FavoritesAdapter;
+import com.example.eventfinder.Adapters.SearchEventListAdapter;
+import com.example.eventfinder.DataClasses.SearchResponse;
+import com.example.eventfinder.Helpers.SharedPreferencesAccessHelper;
+import com.example.eventfinder.ViewModels.FavItemsViewModel;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FavoritesFragment#newInstance} factory method to
+ * Use the {@link FavoritesFragment #newInstance} factory method to
  * create an instance of this fragment.
  */
 public class FavoritesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FavoritesFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FavoritesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FavoritesFragment newInstance(String param1, String param2) {
-        FavoritesFragment fragment = new FavoritesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    private FavoritesAdapter favoritesAdapter;
+    private RecyclerView recyclerView;
+    private TextView noFavsTV;
+    private ProgressBar progressBar;
+    private SharedPreferencesAccessHelper sharedPreferencesAccessHelper;
+    private View rootView;
+    private FavItemsViewModel favItemsViewModel;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+
+        rootView =  inflater.inflate(R.layout.fragment_favorites, container, false);
+
+        favItemsViewModel = new ViewModelProvider(requireActivity()).get(FavItemsViewModel.class);
+
+        favItemsViewModel.isFavListEmpty.observe(getViewLifecycleOwner(), myArrayList -> {
+            if (myArrayList) {
+                Toast.makeText(getActivity(), "VIew Model", Toast.LENGTH_SHORT).show();
+                recyclerView.setVisibility(View.GONE);
+                noFavsTV.setVisibility(View.VISIBLE);
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+        rootView.addOnAttachStateChangeListener(new View.OnAttachStateChangeListener() {
+            @Override
+            public void onViewAttachedToWindow(View view) {
+
+                // Refresh the fragment as needed
+//                Toast.makeText(view.getContext(), "Attached", Toast.LENGTH_SHORT).show();
+                recyclerView.setVisibility(View.GONE);
+                noFavsTV.setVisibility(View.GONE);
+                progressBar.setVisibility(View.VISIBLE);
+                populateFavList();
+            }
+
+            @Override
+            public void onViewDetachedFromWindow(View view) {
+                // Do nothing
+            }
+        });
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//        Toast.makeText(getActivity(), "Favs", Toast.LENGTH_SHORT).show();
+
+        favoritesAdapter = new FavoritesAdapter(view.getContext(), favItemsViewModel);
+        sharedPreferencesAccessHelper = new SharedPreferencesAccessHelper(view.getContext());
+
+        recyclerView = view.findViewById(R.id.favRecyclerView);
+        recyclerView.setAdapter(favoritesAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        noFavsTV = view.findViewById(R.id.noFavsMessage);
+        progressBar = view.findViewById(R.id.progressBar_Fav);
+
+        recyclerView.setVisibility(View.GONE);
+        noFavsTV.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+        populateFavList();
+
+    }
+
+
+    private void populateFavList() {
+
+        Log.d("SHARED PREF", "Checking Shred Prefs");
+        ArrayList<SearchResponse> searchResponses= sharedPreferencesAccessHelper.getItemsFromTable();
+
+        Log.d("SHARED PREF RESPONSE", "SIZE = "+searchResponses.size());
+        favoritesAdapter.setData(searchResponses);
+//        favoritesAdapter.notifyDataSetChanged();
+
+        if(searchResponses.size() == 0){
+            progressBar.setVisibility(View.GONE);
+            noFavsTV.setVisibility(View.VISIBLE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            recyclerView.setVisibility(View.VISIBLE);
+        }
     }
 }
