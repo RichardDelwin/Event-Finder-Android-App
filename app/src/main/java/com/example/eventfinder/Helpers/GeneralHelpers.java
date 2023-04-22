2c57b9e9da148495e4b7bb5f30c7275171274754
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-04-22 02:29:53 -0700

    [NEW] Formatting for event details response

--- app/src/main/java/com/example/eventfinder/Helpers/GeneralHelpers.java
+++ app/src/main/java/com/example/eventfinder/Helpers/GeneralHelpers.java
@@ -1,8 +1,14 @@
 package com.example.eventfinder.Helpers;
 
+import android.text.TextUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class GeneralHelpers {
 
@@ -14,11 +20,53 @@ public class GeneralHelpers {
         return date.format(formatter);
     }
 
+    public static String formatDateWithMonthName(String localDate){
+
+        if(localDate==null)
+            return "";
+
+        Date date = null;
+        try {
+            date = new SimpleDateFormat("yyyy-MM-dd").parse(localDate);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Format the date to the desired format
+        String formattedDate = new SimpleDateFormat("MMM dd, yyyy").format(date);
+
+        return formattedDate;
+    }
+
     public static String getFormattedTime(String localTime) {
         if(localTime==null)
             return "";
         LocalTime time = LocalTime.parse(localTime);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("h:mm a");
         return time.format(formatter);
+    }
+
+    public static String formatArtists(ArrayList<String> artistList){
+        return TextUtils.join(" | ", artistList);
+    }
+
+    public static boolean valueExists(String value){
+        return value != null && value != "";
+    }
+
+    public static String formatPriceRange(double min_double, double max_double){
+
+        String min = String.valueOf(min_double);
+        String max = String.valueOf(max_double);
+
+        if(valueExists(min) && valueExists(max)){
+            return min + " - " + max + " (USD)";
+        } else if (valueExists(min) && !valueExists(max)) {
+            return min + " - " + min + " (USD)";
+        } else if (!valueExists(min) && valueExists(max)) {
+            return max + " - " + max + " (USD)";
+        }else{
+            return "";
+        }
     }
 }
