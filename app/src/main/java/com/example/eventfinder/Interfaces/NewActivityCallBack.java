7f08547102c01db38c6e3972d825bc5d0196e73d
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2023-04-27 19:12:02 -0700

    [CHANGE] Updated argument list

--- app/src/main/java/com/example/eventfinder/Interfaces/NewActivityCallBack.java
+++ app/src/main/java/com/example/eventfinder/Interfaces/NewActivityCallBack.java
@@ -3,5 +3,5 @@ package com.example.eventfinder.Interfaces;
 import com.example.eventfinder.DataClasses.SearchResponse;
 
 public interface NewActivityCallBack<T> {
-    void onButtonClick(T data);
+    void onButtonClick(T data, T data2, SearchResponse response);
 }
